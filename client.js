45af97e42ea27955d64576ab1ab3ae3dcbcea935
Shong Lee <Shonglee17@gmail.com>
2022-12-10 14:26:52 -0600

    added in post/get routes. need to debug the Cannot GET/ message on localport

--- client.js
+++ client.js
@@ -1,5 +1,42 @@
 $(document).ready(onReady)
+let results = 0
 
 function onReady(){
-    console.log('everything sourced in');
+    $('#equalButton').on('click', submittedInputs)
+    
 }
+
+function submittedInputs(){
+
+    let firstInput = $('#firstInput').val()
+    let secondInput = $('#secondInput').val()
+    let operator = $('.operatorButton').text()
+    let resultsOutcome = results
+
+    let values = {
+        firstVal: firstInput,
+        secondVal: secondInput,
+        calcOperator: operator,
+        finalResult: resultsOutcome,
+    }
+    console.log(values);
+}
+
+function outgoingServerInputs(){
+    $.ajax({
+        url: '/outgoingServerInputs',
+        method: 'post', // send the data to the server
+        data: submittedInputs
+      }).then((res) => { //puts the server message in the browers console
+        console.log(res);
+      })
+}
+
+function renderResults() {
+    $.ajax({
+      url: '/outgoingResults',
+      method: 'get',
+    }).then((res) => {
+     console.log(res);
+    })
+  }
--- server/server.js
+++ server/server.js
@@ -4,10 +4,24 @@ const app = express()
 const PORT = 5000
 app.use(express.static('server/public'));
 
+let submittedEquations = []
 
+function solvingEquation(){ 
+   submittedEquations.finalResult = submittedEquations.Number(firstVal) + submittedEquations.calcOperator + submittedEquations.Number(secondVal)  
+    return submittedEquations.finalResult
+}
 
+app.post('/incomingServerInputs', (req, res) => {
+    
+    submittedEquations.push(req.body);
+    res.sendStatus(201); //tells client the array was created
+    console.log(submittedEquations);
+})
 
-
+app.get('/outgoingResults', (req, res) => {
+    console.log('the results being sent back');
+    res.send(solvingEquation(submittedEquations)); //tells client the array was created
+  })
 
 
 
