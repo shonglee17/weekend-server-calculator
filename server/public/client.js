9b1b09e1cd589454c17651deea62625d4ecd66e9
Shong Lee <Shonglee17@gmail.com>
2022-12-11 12:29:18 -0600

    finished conditionals on server and client side

--- server/public/client.js
+++ server/public/client.js
@@ -1,32 +1,44 @@
 $(document).ready(onReady)
-let results = 0
 
+let values = {
+  firstVal: '',
+  secondVal: '',
+  calcOperator: '',
+  finalResult: ''
+}
 function onReady(){
     $('#equalButton').on('click', submittedInputs)
-    
+    $(`#clearButton`).on('click', clearHistory)
+    $('#addButton').on('click', addOperator)
+    $('#subtractButton').on('click', subtractOperator)
+    $('#multiplyButton').on('click', multiplyOperator)
+    $('#divideButton').on('click', divideOperator)
 }
 
 function submittedInputs(){
-
     let firstInput = $('#firstInput').val()
     let secondInput = $('#secondInput').val()
-    let operator = $('.operatorButton').text()
-    let resultsOutcome = results
+    // let operator = $('#addButton').text()
+    // let results = 0
 
-    let values = {
-        firstVal: firstInput,
-        secondVal: secondInput,
-        calcOperator: operator,
-        finalResult: resultsOutcome,
-    }
+    // let values = {
+    //     firstVal: firstInput,
+    //     secondVal: secondInput,
+    //     calcOperator: operator,
+    //     finalResult: results,
+    // }
+    values.firstVal = firstInput
+    values.secondVal = secondInput
     console.log(values);
+    outgoingServerInputs(values)
+    renderResults()
 }
 
-function outgoingServerInputs(){
+function outgoingServerInputs(values){
     $.ajax({
         url: '/outgoingServerInputs',
         method: 'post', // send the data to the server
-        data: submittedInputs
+        data: values
       }).then((res) => { //puts the server message in the browers console
         console.log(res);
       })
@@ -37,6 +49,28 @@ function renderResults() {
       url: '/outgoingResults',
       method: 'get',
     }).then((res) => {
-     console.log(res);
+      $('#lineResults').empty()
+      for (let results of res)
+     $('#lineResults').append(`
+      <li>
+        ${results.firstVal} ${results.calcOperator} ${results.secondVal} = ${results.finalResult}
+      </li>
+     `)
     })
   }
+
+function addOperator(){
+  values.calcOperator = '+'
+}
+
+function subtractOperator(){
+  values.calcOperator = '-'
+}
+
+function multiplyOperator(){
+  values.calcOperator = '*'
+}
+
+function divideOperator(){
+  values.calcOperator = '/'
+}
--- server/server.js
+++ server/server.js
@@ -3,24 +3,41 @@ const bodyParser = require('body-parser')
 const app = express()
 const PORT = 5000
 app.use(express.static('server/public'));
-
+app.use(bodyParser.urlencoded({extended:true}))
 let submittedEquations = []
+// let finalResult = []
 
-function solvingEquation(){ 
-   submittedEquations.finalResult = submittedEquations.Number(firstVal) + submittedEquations.calcOperator + submittedEquations.Number(secondVal)  
-    return submittedEquations.finalResult
-}
+ 
+    
+
+
 
 app.post('/outgoingServerInputs', (req, res) => {
-    
-    submittedEquations.push(req.body);
-    res.sendStatus(201); //tells client the array was created
+    let values = req.body
+    console.log(values.calcOperator);
+    if(values.calcOperator === '+'){
+       values.finalResult = Number(values.firstVal) + Number(values.secondVal) 
+        console.log(values.finalResult);
+    }
+    else if(values.calcOperator === '-') {
+        values.finalResult = Number(values.firstVal) - Number(values.secondVal)
+    }
+    else if(values.calcOperator === '*'){
+        values.finalResult = Number(values.firstVal) * Number(values.secondVal)
+    }
+    else{
+        values.finalResult = Number(values.firstVal) / Number(values.secondVal)
+    }
+
+    submittedEquations.push(values);
     console.log(submittedEquations);
+    res.sendStatus(201); //tells client the array was created
+    
 })
 
 app.get('/outgoingResults', (req, res) => {
     console.log('the results being sent back');
-    res.send(solvingEquation(submittedEquations)); //tells client the array was created
+    res.send(submittedEquations) //tells client the array was created
   })
 
 
